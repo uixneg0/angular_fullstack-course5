f4b25e32254f9d5d461069d5cf7b7d024d17746e
uixneg0 <uixneg0@gmail.com>
2023-02-25 18:46:14 -0600

    assignment 3

---
+++ assignments/assignment3/assignment3-starter-code/app.js
@@ -0,0 +1,95 @@
+(function () {
+    'use strict';
+
+    angular.module('App', [])
+        .controller('NarrowItDownController', NarrowItDownController)
+        .service('MenuSearchService', MenuSearchService)
+        .directive('foundItems', foundItemsDirective)
+        .constant('ApiUrl', "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items.json");
+
+
+    function foundItemsDirective() {
+        var ddo = {
+            templateUrl: 'foundItems.html',
+            controller: NarrowItDownController,
+            controllerAs: 'controller',
+            scope: {
+                items: '<',
+                onRemove: '&',
+            },
+            bindToController: true,
+        };
+
+        return ddo;
+    }
+
+    NarrowItDownController.$inject = ['MenuSearchService'];
+
+    function NarrowItDownController(MenuSearchService) {
+        this.found = [];
+        this.searchTerm = "";
+        this.service = MenuSearchService;
+
+        this.findItems = function () {
+            var promise = this.service.getMatchedMenuItems(this.searchTerm);
+            promise.then((result) => {
+                this.found = result;
+                console.log(this.found);
+            });
+        }
+
+        this.removeItem = function (index) {
+            this.found.splice(index, 1);
+        }
+
+        this.test = () => {
+            console.log(this.found);
+        }
+    }
+
+    MenuSearchService.$inject = ['$http', 'ApiUrl'];
+
+    function MenuSearchService($http, ApiUrl) {
+        this.getMatchedMenuItems = function (searchTerm) {
+            searchTerm = searchTerm.toLowerCase();
+            var promise = this.getMenuItems()
+                .then(function (response) {
+                    var matchedItems = [];
+                    var responseData = response.data;
+                    var keys = Object.keys(responseData);
+                    keys.forEach(key => {
+                        var menuItems = responseData[key]["menu_items"];
+                        menuItems.forEach(menuItem => {
+                            if (menuItem.description.toLowerCase().includes(searchTerm)) {
+                                matchedItems.push(new Item(menuItem.name, menuItem.short_name, menuItem.description))
+                            }
+                        });
+                    });
+                    return matchedItems;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            return promise;
+        };
+
+        this.getMenuItems = function () {
+            return $http({
+                method: "GET",
+                url: (ApiUrl),
+                response: "json"
+            });
+        };
+
+    }
+
+
+    class Item {
+        constructor(name, shortName, description) {
+            this.name = name;
+            this.shortName = shortName;
+            this.description = description;
+        }
+    }
+
+})();
